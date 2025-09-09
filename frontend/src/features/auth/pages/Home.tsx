adb5c83de5ec85368b33c639c2a2e4e9e774b597
agnkos <101097155+agnkos@users.noreply.github.com>
2025-09-09 08:27:17 +0200

    Feat/desktop UI (#1)
    * added signup desktop view & styling
    
    * login page desktop added
    
    * updated home page ui

--- frontend/src/features/auth/pages/Home.tsx
+++ frontend/src/features/auth/pages/Home.tsx
@@ -3,8 +3,8 @@ import AuthButton from "../components/AuthButton"
 
 const Home = () => {
   return (
-    <>
-      <div className="h-screen flex flex-col px-8 py-6 text-center items-center justify-center bg-home-bg">
+    <div className="h-screen max-h-screen flex flex-col px-8 py-6 text-center items-center justify-center bg-home-bg">
+      <div className="flex flex-col justify-center items-center w-full max-h-[95%] grow-1 ">
         <h1 className="text-4xl max-[320px]:text-3xl leading-10 mb-4 font-extrabold text-text">
           Welcome to{" "}
           <span className="text-main-accent font-montserrat">Bookshelf</span>
@@ -15,20 +15,22 @@ const Home = () => {
         <img
           src={BooksImg}
           alt="a stack of books illustration"
-          className=" mx-auto max-h-[50svh]"
+          className=" mx-auto max-h-[50svh] max-[400px]:max-h-[35svh]"
         />
-        <div className="w-11/12 flex flex-col items-center gap-4 max-w-sm">
+        <div className="w-11/12 flex flex-col items-center gap-4 mb-4 max-w-sm">
           <AuthButton text="Log In" link="/login" />
           <AuthButton text="Sign Up" link="/signup" />
         </div>
       </div>
-      <p className="text-xs text-main-accent fixed bottom-4 left-2/4 -translate-x-2/4">
-        Image by
-        <a href="https://pl.freepik.com/darmowe-wektory/recznie-rysowane-plaski-stos-ksiazek-ilustracji_24307396.htm#query=book&position=4&from_view=search&track=sph&uuid=b4d24fca-f528-4726-aa10-74d391a4ced2">
-          Freepik
-        </a>
+      <p className="flex grow md:grow-0 items-end text-xs text-main-accent md:mt-4 bg-light-bg">
+        <div>
+          Image by
+          <a href="https://pl.freepik.com/darmowe-wektory/recznie-rysowane-plaski-stos-ksiazek-ilustracji_24307396.htm#query=book&position=4&from_view=search&track=sph&uuid=b4d24fca-f528-4726-aa10-74d391a4ced2">
+            &nbsp;Freepik
+          </a>
+        </div>
       </p>
-    </>
+    </div>
   )
 }
 
--- frontend/src/features/auth/pages/Login.tsx
+++ frontend/src/features/auth/pages/Login.tsx
@@ -17,70 +17,78 @@ const Login = () => {
   })
 
   return (
-    <div className="h-screen flex flex-col justify-center px-8 py-6 text-center items-center bg-light-bg">
-      <img
-        src={ShelfImg}
-        alt="a shelf with books illustration"
-        className="max-w-full max-h-[35%] mx-auto"
-      />
-      <h1 className="text-4xl max-[320px]:text-3xl mb-2 font-bold text-main-accent">
-        BookJourney
-      </h1>
-      <p className="text-xl font-medium mb-4">Log In</p>
-      {/* {location.state?.message && <h3>{location.state.message}</h3>} */}
+    <div className="h-screen max-h-screen flex flex-col justify-center items-center px-8 py-6 text-center bg-light-bg">
+      <div className="flex flex-col justify-center w-full p-0 md:p-6 max-h-[95%] mid:max-h-[100%] md:grid md:grid-cols-2 md:items-center md:border md:border-main-accent md:rounded-lg max-w-4xl md:shadow-[4px_4px_0px_rgb(65,174,153)]">
+        <img
+          src={ShelfImg}
+          alt="a shelf with books illustration"
+          className="max-w-full max-h-[35%] mx-auto md:max-h-[80%]"
+        />
+        <div className="w-full">
+          <h1 className="text-4xl max-[400px]:text-3xl mb-2 font-bold text-main-accent">
+            Bookshelf
+          </h1>
+          <p className="text-xl max-[400px]:text-base font-medium mb-4">
+            Log In
+          </p>
+          {/* {location.state?.message && <h3>{location.state.message}</h3>} */}
 
-      <Formik
-        initialValues={initialValues}
-        validationSchema={loginValidationSchema}
-        onSubmit={onSubmit}
-      >
-        {({ values, isSubmitting }) => {
-          return (
-            <Form className="w-11/12 flex flex-col items-center gap-4 max-w-md mx-auto">
-              <div className="w-full">
-                <Field
-                  type="text"
-                  value={values.username}
-                  name="username"
-                  placeholder="Username"
-                  className="w-11/12 mb-1 px-4 py-2 rounded-md border"
-                />
-                <ErrorMessage
-                  name="username"
-                  component="div"
-                  className="text-sm text-red-500"
-                />
-              </div>
-              <div className="w-full">
-                <Field
-                  type="password"
-                  value={values.password}
-                  name="password"
-                  placeholder="Password"
-                  className="w-11/12 mb-1 px-4 py-2 rounded-md border"
-                />
-                <ErrorMessage
-                  name="password"
-                  component="div"
-                  className="text-sm text-red-500"
-                />
-              </div>
-              <button
-                type="submit"
-                className="w-11/12 px-4 py-2 mt-2 text-center bg-lighter-accent hover:bg-main-accent-hover text-light-bg font-semibold rounded-md"
-                disabled={isSubmitting}
-              >
-                Log In
-              </button>
-            </Form>
-          )
-        }}
-      </Formik>
-      <p className="text-xs text-main-accent fixed bottom-4 left-2/4 -translate-x-2/4">
-        Image by
-        <a href="https://pl.freepik.com/darmowe-wektory/recznie-rysowane-ilustracja-kregoslupa-ksiazki-o-plaskiej-konstrukcji_24307294.htm#from_view=detail_serie">
-          Freepik
-        </a>
+          <Formik
+            initialValues={initialValues}
+            validationSchema={loginValidationSchema}
+            onSubmit={onSubmit}
+          >
+            {({ values, isSubmitting }) => {
+              return (
+                <Form className="w-11/12 flex flex-col items-center gap-4 max-w-md mx-auto">
+                  <div className="w-full">
+                    <Field
+                      type="text"
+                      value={values.username}
+                      name="username"
+                      placeholder="Username"
+                      className="w-11/12 mb-1 px-4 py-2 rounded-md border"
+                    />
+                    <ErrorMessage
+                      name="username"
+                      component="div"
+                      className="text-sm text-red-500"
+                    />
+                  </div>
+                  <div className="w-full">
+                    <Field
+                      type="password"
+                      value={values.password}
+                      name="password"
+                      placeholder="Password"
+                      className="w-11/12 mb-1 px-4 py-2 rounded-md border"
+                    />
+                    <ErrorMessage
+                      name="password"
+                      component="div"
+                      className="text-sm text-red-500"
+                    />
+                  </div>
+                  <button
+                    type="submit"
+                    className="w-11/12 px-4 py-2 mt-2 text-center bg-lighter-accent hover:bg-main-accent-hover text-light-bg font-semibold rounded-md"
+                    disabled={isSubmitting}
+                  >
+                    Log In
+                  </button>
+                </Form>
+              )
+            }}
+          </Formik>
+        </div>
+      </div>
+      <p className="flex grow md:grow-0 items-end text-xs text-main-accent md:mt-4 bg-light-bg">
+        <div>
+          Image by
+          <a href="https://pl.freepik.com/darmowe-wektory/recznie-rysowane-ilustracja-kregoslupa-ksiazki-o-plaskiej-konstrukcji_24307294.htm#from_view=detail_serie">
+            &nbsp; Freepik
+          </a>
+        </div>
       </p>
     </div>
   )
