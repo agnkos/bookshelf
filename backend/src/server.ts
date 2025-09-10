3cc82b19c99196299434610a02ab89060e092828
agnkos <agnieszkakosiedowska@gmail.com>
2025-09-10 10:57:32 +0200

    ts && prisma && server setup

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,7 +2,7 @@ import express from "express"
 import cors from "cors"
 import dotenv from "dotenv"
 
-import userRoutes from "./routes/userRoutes"
+import authRoutes from "./routes/auth.routes"
 
 dotenv.config()
 
@@ -11,13 +11,13 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-app.use("/api/users", userRoutes)
+app.use("/auth", authRoutes)
 
 app.get("/", (req, res) => {
-  res.send("API is running ")
+  res.send("backend is running ")
 })
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
-  console.log(`✅ Server is running on port ${PORT}`)
+  console.log(`Server is running on port ${PORT}`)
 })
